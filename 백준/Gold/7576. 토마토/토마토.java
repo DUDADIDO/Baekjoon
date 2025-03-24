d5eaa53cbd5a64b96222595c8fd1acab5d08f5b7
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2025-03-25 00:02:01 +0900

    [Gold V] Title: 토마토, Time: 1592 ms, Memory: 346500 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -49,7 +49,6 @@ public class Main {
 //            }
 //            System.out.println();
 //        }
-
         boolean flag = false;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
