6ae540bf3f2047bf56f5b6370f55c56754da2dca
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2025-08-18 22:44:26 +0900

    [level 2] Title: 올바른 괄호, Time: 7.09 ms, Memory: 55 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                cnt++;
+            }
+            else {
+                if(cnt <= 0) {
+                    answer = false;
+                    break;
+                }
+                else {
+                    cnt--;
+                }
+            }
+        }
+        if(cnt != 0)
+            answer = false;
+        return answer;
+    }
+}
