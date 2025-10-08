68908ee6c90580dd38e817635e90ef4604de0c18
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2025-10-08 23:30:46 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        int n = phone_number.length();
+        
+        for(int i=0; i<n; i++) {
+            if(i < n - 4) {
+                sb.append("*");
+            }
+            else {
+                sb.append(phone_number.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
