dfac000c51fe1caf114da725480efcceef66b06c
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2025-11-19 10:19:21 +0900

    [D1] Title: 평균값 구하기, Time: 90 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+            for(int i=0; i<10; i++) {
+                sum += sc.nextInt();
+            }
+            double avg = sum / 10.0;
+            int avgInt = sum / 10;
+            if(avg - avgInt >= 0.5) {
+                avgInt = avgInt + 1;
+            }
+            System.out.println("#" + test_case + " " + avgInt);
+
+        }
+    }
+}
