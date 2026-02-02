38ea666b2536a57977c04355aa59be9f2fafeeb3
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-02 12:22:21 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 77 ms, Memory: 24,704 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=10; tc++) {
+			StringTokenizer st;
+			List<Integer> list = new ArrayList<>();
+			sb.append("#").append(tc).append(" ");
+			
+			int length = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<length; i++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+			int commandLength = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<commandLength; i++) {
+				st.nextToken();
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				for(int j=0; j<y; j++) {
+					list.add((x+j), Integer.parseInt(st.nextToken()));
+				}
+			}
+			
+			for(int i=0; i<10; i++) {
+				sb.append(list.get(i)).append(" ");
+			}
+			
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+	}
+}
