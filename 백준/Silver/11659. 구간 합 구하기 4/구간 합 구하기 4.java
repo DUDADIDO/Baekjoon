a85a64cba4fad94dbea58cedc3bfff04cd43bf2b
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-03 14:37:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 484 ms, Memory: 52708 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] nArr = new int[N+1];
-        st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++) {
-            nArr[i] = nArr[i-1] + Integer.parseInt(st.nextToken());
-        }
-
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(nArr[b] - nArr[a-1]).append("\n");
-        }
-        
-        System.out.print(sb);
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] nArr = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		int sum = 0;
+		for(int i=1; i<=N; i++) {
+			sum += Integer.parseInt(st.nextToken());
+			nArr[i] = sum;
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+//			System.out.println("start: " + start + "   end: " + end);
+			int result = nArr[end] - nArr[start-1];
+			sb.append(result).append("\n");
+		}
+		System.out.println(sb);
+		
+		
+	}
 }
