2365f5b357b18a173976e2d0a52c0d0e840b3fa8
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-03 11:33:47 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long[] dp = new long[101];
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		dp[4] = 2;
+		dp[5] = 2;
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			long answer = 0;
+			if(dp[N] > 0) {
+				answer = dp[N];
+			}
+			else {
+				for(int i=6; i<=N; i++) {
+					dp[i] = dp[i-1] + dp[i-5];
+				}
+				answer = dp[N];
+			}
+			
+			System.out.println(answer);
+		
+		}
+	}
+}
