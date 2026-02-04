c5d5221093985804b4ca4ce462b59927f61ba7ce
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-04 15:38:47 +0900

    [Silver III] Title: N과 M (4), Time: 120 ms, Memory: 16792 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] nums, ans, visit;
+	static StringBuilder sb = new StringBuilder();
+	
+	static void dfs(int L) {
+		if(L == M) {
+			for(int num : ans)
+				sb.append(num).append(" ");
+			sb.append("\n");
+		}
+		else {
+			for(int i=0; i<N; i++) {
+				if(L == 0) {
+					ans[L] = nums[i];
+					dfs(L+1);
+				}
+				else {
+					if(nums[i] >= ans[L-1]) {
+						ans[L] = nums[i];
+						dfs(L+1);
+					}
+				}
+				
+				
+
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+		ans = new int[M];
+		visit = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			nums[i] = i+1;
+		}
+		
+		dfs(0);
+		System.out.println(sb);
+		
+	}
+}
