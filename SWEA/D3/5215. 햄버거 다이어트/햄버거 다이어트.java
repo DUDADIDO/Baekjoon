a82e50bf9fda4a695a35319e0c621de1d539db5f
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-06 12:04:49 +0900

    [D3] Title: 햄버거 다이어트, Time: 144 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,49 +1,48 @@
-import java.util.Scanner;
-class food {
-    public int kalo;
-    public int score;
-    public food(int kalo, int score) {
-        this.kalo = kalo;
-        this.score = score;
-    }
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-
-            
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int N = sc.nextInt();
-            int L = sc.nextInt();
-            food[] foods = new food[N];
-            int[] dp = new int[L +1 ];
-            for(int i=0; i<N; i++) {
-                int score = sc.nextInt();
-                int kalo = sc.nextInt();
-                foods[i] = new food(kalo, score);
-            }
-            
-            for(int i=0; i<N; i++) {
-                int currentKalo = foods[i].kalo;
-                int currentScore = foods[i].score;
-                
-                for(int j=L; j>=currentKalo; j--) {
-                    dp[j] = Math.max(dp[j], dp[j-currentKalo] + currentScore);
-                }
-
-            }
-            System.out.println("#" + test_case + " " + dp[L]);             
-                                     
-            
-            
-
+public class Solution {
+	static int N, L, maxScore;
+	static int[] kcal, scores;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			sb.append("#").append(tc).append(" ");
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			
+			kcal = new int[N];
+			scores = new int[N];
+			
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				scores[i] = Integer.parseInt(st.nextToken());
+				kcal[i] = Integer.parseInt(st.nextToken());
+			}
+			maxScore = 0;
+			dfs(0, 0, 0);
+			sb.append(maxScore).append("\n");
+			
 		}
+		System.out.println(sb);
+
+	}
+	static void dfs(int idx, int scoreSum, int kcalSum) {
+		if(kcalSum > L)
+			return;
+		if(idx == N) {
+			maxScore = Math.max(scoreSum, maxScore);
+			return;
+		}
+		
+		dfs(idx+1, scoreSum+scores[idx], kcalSum+kcal[idx]); // 고름
+		dfs(idx+1, scoreSum, kcalSum); // 고르지 않음
+		
 	}
 }
