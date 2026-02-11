639cd86a9a51ff6140220851ab3be5f597056934
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-02-11 11:23:35 +0900

    [Gold IV] Title: 단어 수학, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+// 현재 스펠링의 위치가 앞서있으면 숫자 매기기(9부터 순서대로)
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int cnt = 9;
+		int n = Integer.parseInt(br.readLine());
+		Map<Character, Integer> hm = new HashMap<>();
+		
+		for(int i=0; i<n; i++)  {
+//			pq.add(new word(br.readLine()));
+			String word = br.readLine();
+			int weight = (int)Math.pow(10, word.length()-1);
+			for(int idx=0; idx<word.length(); idx++) {
+				char c  = word.charAt(idx);
+				hm.put(c, hm.getOrDefault(c, 0) + weight);
+				weight /= 10;
+			}
+		}
+		List<Integer> weights = new ArrayList<>(hm.values());
+		weights.sort(Collections.reverseOrder());
+		for(int i=0; i<weights.size(); i++) {
+			sum += weights.get(i)*cnt--;
+		}
+//		while(pq.peek().weight > 0) {
+//			word curWord = pq.poll();
+//			if(hm.get(curWord.str.charAt(curWord.index)) == null) {
+//				hm.put(curWord.str.charAt(curWord.index), cnt--);
+//			}
+//			int num = hm.get(curWord.str.charAt(curWord.index));
+//			sum += (num * curWord.weight);
+//			curWord.index++;
+//			curWord.weight = curWord.weight / 10;
+//			pq.add(curWord);
+//		}	
+		System.out.println(sum);
+		
+	}
+}
