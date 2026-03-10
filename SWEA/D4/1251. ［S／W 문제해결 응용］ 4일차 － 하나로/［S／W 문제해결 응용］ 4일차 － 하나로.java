cdc11581939c160584ee21bbc6f1fdb3a818aaa0
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-10 12:37:57 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 534 ms, Memory: 117,684 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	static class Edge implements Comparable<Edge> {
-		int start, target;
+		int target;
 		double weight;
 
-		public Edge(int start, int target, double weight) {
+		public Edge(int target, double weight) {
 			super();
-			this.start = start;
 			this.target = target;
 			this.weight = weight;
 		}
@@ -27,8 +27,7 @@ public class Solution {
 	}
 
 	static int v;
-	static int[] parents;
-	static List<Edge> list;
+	static List<List<Edge>> graph;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -40,68 +39,55 @@ public class Solution {
 			v = Integer.parseInt(st.nextToken());
 
 			int[][] islands = new int[v][2];
+			graph = new ArrayList<>();
 			
-			list = new ArrayList<>();
-			parents = new int[v + 1];
-			Arrays.fill(parents, -1);
 			st = new StringTokenizer(br.readLine());
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
 			for(int i=0; i<v; i++) {
 				islands[i][0] = Integer.parseInt(st.nextToken());
+				islands[i][1] = Integer.parseInt(st2.nextToken());
+				graph.add(new ArrayList<>());
 			}
-			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<v; i++) {
-				islands[i][1] = Integer.parseInt(st.nextToken());
-			}
+
 			double e = Double.parseDouble(br.readLine());
 			for(int i=0; i<v-1; i++) {
 				for(int j=i; j<v; j++) {
 					double dist = getDistance(islands[i][0], islands[i][1], islands[j][0], islands[j][1]);
 					double weight = e * Math.pow(dist, 2);
-					list.add(new Edge(i, j, weight));
+					graph.get(i).add(new Edge(j, weight));
+					graph.get(j).add(new Edge(i, weight));
 				}
 			}
 			
-			long result = Math.round(kruskal());
+			long result = Math.round(prim(0));
 			sb.append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	static double kruskal() {
-		Collections.sort(list);
+	static double prim(int start) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o1.weight, o2.weight));
+		boolean[] visited = new boolean[v];
+		visited[start] = true;
 		double result = 0;
 		int count = 0;
+		for(Edge edge : graph.get(start)) pq.offer(edge);
 		
-		for(Edge edge : list) {
-			if(!union(edge.start, edge.target)) continue;
-			result += edge.weight;
-			count++;
+		while(!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			if(visited[cur.target]) continue;
+			
+			visited[cur.target] = true;
+			result += cur.weight;
+			count ++;
 			
 			if(count == v-1) return result;
+			
+			for(Edge edge : graph.get(cur.target)) pq.offer(edge);
 		}
 		return -1;
-	}
-
-	static int find(int a) {
-		if (parents[a] < 0)
-			return a; // 음수면 해당 요소가 집합의 루트
-		return parents[a] = find(parents[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-		if (rootA == rootB) // 두 요소가 같은 집합에 속해있다면 union 수행 x
-			return false;
-
-		if (parents[rootA] > parents[rootB]) { // 항상 좌측의 집합이 우측보다 더 크도록 조정
-			int tmp = rootA;
-			rootA = rootB;
-			rootB = tmp;
-		}
-		parents[rootA] += parents[rootB]; // 좌측 집합의 크기에 우측 집합의 크기를 더한다.
-		parents[rootB] = rootA; // 우측 집합의 루트를 좌측 집합의 루트로 변경
-		return true;
+		
+		
 	}
 	
 	static double getDistance(int x1, int y1, int x2, int y2) {
