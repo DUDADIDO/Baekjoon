f3e343a3b6913696fd53ead187bbc0a1317e081d
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-10 09:10:05 +0900

    [Silver IV] Title: 상근이의 여행, Time: 324 ms, Memory: 45848 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());	
+			int m = Integer.parseInt(st.nextToken());
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+			}
+			System.out.println(n-1);
+		}
+		
+		
+		
+	}
+}
