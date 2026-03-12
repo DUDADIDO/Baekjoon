4302652a1aaa1c6f5e0d9147e2126cd94d3e035a
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-12 17:51:54 +0900

    [Gold IV] Title: 최단경로, Time: 2292 ms, Memory: 108452 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
+class Node{
 	int index;
 	int weight;
 	
@@ -15,26 +15,25 @@ class Node implements Comparable<Node>{
 		this.index = index;
 		this.weight = weight;
 	}
-	@Override
-	public int compareTo(Node o) {
-		// TODO Auto-generated method stub
-		return this.weight - o.weight;
-	}
+
 }
 
 public class Main {
 	static int[] distance;
 	static List<List<Node>> graph = new ArrayList<>();
+	static int n;
+	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
 		int start = Integer.parseInt(br.readLine());
 		distance = new int[n+1];
+		visited = new boolean[n+1];
 		Arrays.fill(distance, Integer.MAX_VALUE);
 		for(int i=0; i<=n; i++) {
 			graph.add(new ArrayList<>());
@@ -57,24 +56,44 @@ public class Main {
 		}
 	}
 	static void dijksatra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
 		distance[start] = 0;
-		pq.add(new Node(start, 0));
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
-			int curIdx = cur.index;
-			int curWeight = cur.weight;
-			if(curWeight > distance[curIdx]) {
-				continue;
-			}
-			for(Node neighbor : graph.get(curIdx)) {
-				int cost = distance[curIdx] + neighbor.weight;
-				if(cost < distance[neighbor.index]) {
-					distance[neighbor.index] = cost;
-					pq.add(new Node(neighbor.index, cost));
+		
+		for(int i=0; i<n; i++) {
+			int minDistance = Integer.MAX_VALUE;
+			int curIdx = -1;
+			
+			for(int j=1; j<=n; j++) {
+				if(!visited[j] && distance[j] < minDistance) {
+					minDistance = distance[j];
+					curIdx = j;
 				}
-				
+			}
+			if(curIdx == -1) break;
+			
+			visited[curIdx] = true;
+			
+			for(Node node : graph.get(curIdx)) {
+				if(distance[curIdx] + node.weight < distance[node.index]) {
+					distance[node.index] = distance[curIdx] + node.weight;
+				}
 			}
 		}
+		
+//		while(!pq.isEmpty()) {
+//			Node cur = pq.poll();
+//			int curIdx = cur.index;
+//			int curWeight = cur.weight;
+//			if(curWeight > distance[curIdx]) {
+//				continue;
+//			}
+//			for(Node neighbor : graph.get(curIdx)) {
+//				int cost = distance[curIdx] + neighbor.weight;
+//				if(cost < distance[neighbor.index]) {
+//					distance[neighbor.index] = cost;
+//					pq.add(new Node(neighbor.index, cost));
+//				}
+//				
+//			}
+//		}
 	}
 }
