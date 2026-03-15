595b96316abb684aa297f60a53594d054d43b92a
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-16 01:19:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 532 ms, Memory: 54784 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -7,62 +7,64 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Edge {
-		int target;
-		int weight;
-		
-		Edge(int target, int weight) {
-			this.target = target;
-			this.weight = weight;
-		}
-	}
-	static List<List<Edge>> graph;
-	static boolean[] visited;
-	static int v;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		graph = new ArrayList<>();
-		visited = new boolean[v+1];
-		for(int i=0; i<=v; i++) {
-			graph.add(new ArrayList<>());
-		}
-		for(int i=0; i<e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			
-			graph.get(start).add(new Edge(end, weight));
-			graph.get(end).add(new Edge(start, weight));
-		}
-		long result = prim(1);
-		
-		System.out.println(result);
-		
-	}
-	static long prim(int start) {
-		long result = 0;
-		int count = 0;
-		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) ->  {
-			return o1.weight - o2.weight;
-		});
-		visited[start] = true;
-		for(Edge edge : graph.get(start)) {
-			pq.offer(edge);
-		}
-		while(!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			if(visited[cur.target]) continue;
-			count++;
-			visited[cur.target] = true; 
-			result += cur.weight;
-			if(count == v-1) break;
-			for(Edge edge : graph.get(cur.target)) pq.offer(edge);
-		}
-		return result;
-	}
+    static class Edge implements Comparable<Edge> {
+        int target, weight;
+
+        Edge(int target, int weight) {
+            this.target = target;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+    static int v, e;
+    static List<List<Edge>> graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
+        for(int i=0; i<=v; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0; i<e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int target = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph.get(start).add(new Edge(target, weight));
+            graph.get(target).add(new Edge(start, weight));
+        }
+        prim(1);
+
+    }
+    static void prim(int start) {
+        boolean[] visited = new boolean[v+1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+
+        int cnt = 0;
+        int totalWeight = 0;
+        while(!pq.isEmpty()) {
+            Edge cur = pq.poll();
+
+            if(visited[cur.target]) continue;
+
+            visited[cur.target] = true;
+            totalWeight += cur.weight;
+            cnt++;
+
+            if(cnt == v) break;
+            for(Edge edge : graph.get(cur.target)) {
+                if(visited[edge.target]) continue;
+                pq.offer(edge);
+            }
+        }
+        System.out.println(totalWeight);
+    }
 }
