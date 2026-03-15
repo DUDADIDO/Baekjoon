e2f3d0086e959cf7aad8f23270eae8fe4339f831
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-03-16 01:28:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 588 ms, Memory: 50008 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,15 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static class Edge implements Comparable<Edge> {
-        int target, weight;
+        int start, target, weight;
 
-        Edge(int target, int weight) {
+        public Edge(int start, int target, int weight) {
+            this.start = start;
             this.target = target;
             this.weight = weight;
         }
@@ -21,50 +22,54 @@ public class Main {
         }
     }
     static int v, e;
-    static List<List<Edge>> graph;
+    static int[] parents;
+    static List<Edge> list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        graph = new ArrayList<>();
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+list = new ArrayList<>();
+        parents = new int[v+1];
         for(int i=0; i<=v; i++) {
-            graph.add(new ArrayList<>());
+            parents[i] = i;
         }
+
         for(int i=0; i<e; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int target = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
 
-            graph.get(start).add(new Edge(target, weight));
-            graph.get(target).add(new Edge(start, weight));
+            list.add(new Edge(start, target, weight));
         }
-        prim(1);
-
+        kruskal();
     }
-    static void prim(int start) {
-        boolean[] visited = new boolean[v+1];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
-
+    static void kruskal() {
         int cnt = 0;
         int totalWeight = 0;
-        while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
-
-            if(visited[cur.target]) continue;
-
-            visited[cur.target] = true;
-            totalWeight += cur.weight;
-            cnt++;
-
-            if(cnt == v) break;
-            for(Edge edge : graph.get(cur.target)) {
-                if(visited[edge.target]) continue;
-                pq.offer(edge);
+        Collections.sort(list);
+        for(Edge edge : list) {
+            if(find(edge.start) != find(edge.target)) {
+                union(edge.start, edge.target);
+                totalWeight += edge.weight;
+                cnt++;
             }
+
+            if(cnt == v-1) break;
         }
         System.out.println(totalWeight);
+    }
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA == rootB) return;
+        parents[rootB] = rootA;
+    }
+    static int find(int a) {
+        if(parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
     }
 }
