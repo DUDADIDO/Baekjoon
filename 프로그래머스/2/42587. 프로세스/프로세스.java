0992f0533b61eb9c4f57e623809eedb58a9f68c1
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2026-09-06 00:39:26 +0900

    [level 2] Title: 프로세스, Time: 2.55 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,82 +1,36 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    public class process {
-        public int priority;
-        public int flag;
-        
-        public process(int priority, int flag) {
-            this.priority = priority;
-            this.flag = flag;
-        }
-    }
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        int target = priorities[location];
-        Queue<process> q = new LinkedList<>();
+        int targetIdx = location;
+        Queue<int[]> q = new ArrayDeque<>();
         
-        Integer[] tmp = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Collections.reverseOrder());
-        
-        int index = 0;
-        for(int priority : priorities) {
-            if(index == location) {
-                q.add(new process(priority, 1));
+        for(int i=0; i<priorities.length; i++) {
+            q.offer(new int[]{priorities[i], i});
+        }
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int max = 0;
+            for(int i=0; i<q.size(); i++) {
+                int[] tmp2 = q.poll();
+                if(tmp2[0] > max) max = tmp2[0];
+                q.offer(tmp2);
+            }
+            if(max > tmp[0]) {
+                q.offer(tmp);
             }
             else {
-                q.add(new process(priority, 0));
+                cnt++;
+                if(tmp[1] == location)
+                    answer = cnt;
             }
-            index++;
+                
+            
         }
-        for(int i=0; i<tmp.length; i++) {
-            int cur = tmp[i];
-            if(cur > target) {
-                while(q.peek().priority != cur) {
-                    q.add(q.remove());
-                }
-                q.remove();
-                answer++;
-                
-                
-                // if(q.peek().priority == cur) {
-                //     q.remove();
-                //     answer++;
-                // }
-                // else {
-                //     q.add(q.remove());
-                // }
-            }
-            else if(cur == target) {
-                while(true) {
-                   if(q.peek().priority == target) {
-                       if(q.peek().flag == 1) {
-                           answer++;
-                           return answer;
-                       }
-                       else {
-                           q.remove();
-                           answer++;
-                       }
-                   }
-                    else {
-                        q.add(q.remove());
-                    }
-                }
-                
-                
-                // if(q.peek().priority == target) {
-                //     if(q.peek().flag == 1) {
-                //         answer++;
-                //         return answer;
-                //     }
-                //     else {
-                //         q.add(q.remove());
-                //     }
-                // }
-            }
-        }
+        
         return answer;
     }
 }
